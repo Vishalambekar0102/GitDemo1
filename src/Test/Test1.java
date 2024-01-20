dc8f391e17985e0063eaea4952b790b53d0b1be9
vishalambekar <vishalambekar001@gmail.com>
2024-01-20 08:00:16 +0530

    C1

---
+++ src/Test/Test1.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String Name="vishal";
+		String Surname="Ambekar";
+		
+		if(Name.endsWith("vishal")&&Surname.equals("Ambekar")) {
+			System.out.println("Name and Surname correct");
+		}
+		else {
+			System.out.println("Name and Surname not correct");
+		}
+
+
+	}
+
+}
